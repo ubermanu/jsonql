129d49e5ada05f266768dd2b78ed072fe577693c
ubermanu <manu@paca.happy-dev.fr>
2022-10-04 13:11:33 +0200

    add select tests

---
+++ test/select.test.ts
@@ -0,0 +1,65 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import knuql from '../src/knuql'
+
+const data = {
+  cars: [
+    {
+      brand: 'Ford',
+      model: 'Mustang',
+      year: 1969,
+    },
+    {
+      brand: 'Ford',
+      model: 'F-150',
+      year: 2015,
+    },
+    {
+      brand: 'Dodge',
+      model: 'Charger',
+      year: 1970,
+    },
+  ],
+}
+
+test('Select all the data', () => {
+  const result = knuql('.', data)
+  assert.equal(result, data)
+})
+
+test('Select all the cars', () => {
+  const result = knuql('.cars', data)
+  assert.equal(result, data.cars)
+})
+
+test('Select the first car', () => {
+  const result = knuql('.cars[0]', data)
+  assert.equal(result, data.cars[0])
+})
+
+test('Select the first car brand', () => {
+  const result = knuql('.cars[0].brand', data)
+  assert.equal(result, data.cars[0].brand)
+})
+
+test('Select the last car', () => {
+  const result = knuql('.cars[-1]', data)
+  assert.equal(result, data.cars[data.cars.length - 1])
+})
+
+test('Select all cars with brand Ford', () => {
+  const result = knuql('.cars[brand=Ford]', data)
+  assert.equal(result, [data.cars[0], data.cars[1]])
+})
+
+test('Select all cars with brand Ford and model Mustang', () => {
+  const result = knuql('.cars[brand=Ford][model=Mustang]', data)
+  assert.equal(result, [data.cars[0]])
+})
+
+test('Select all cars with a year greater than 1970', () => {
+  const result = knuql('.cars[year>1970]', data)
+  assert.equal(result, [data.cars[1]])
+})
+
+test.run()
